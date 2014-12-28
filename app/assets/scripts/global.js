a4cc9413e4869d40481ad4ccaa834360ff5c497f
William Butt <william.butt@starcomww.com>
2014-12-27 20:41:13 -0500

    MFK take 2, initial commit

---
+++ app/assets/scripts/global.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var APP = window.APP = window.APP || {};
+
+APP.global = (function(){
+
+    var initGlobalComponents = function() {
+        APP.navigation.init();
+        // APP.footer.init();
+    };
+
+    var initPageComponents = function() {
+        APP.core.controller.init();
+    };
+
+    var init = function() {
+
+        /**
+        * initialize global components
+        */
+        initGlobalComponents();
+
+        /**
+        * initialize components for the current page
+        */
+        initPageComponents();
+        $("#2013results").tablesorter();
+
+        $("#gallery").owlCarousel({
+
+            autoPlay: 3000, //Set AutoPlay to 3 seconds
+
+            items : 2
+            //itemsDesktop : [1199,3],
+            //itemsDesktopSmall : [979,3]
+
+        });
+    };
+
+    /**
+     * interfaces to public functions
+     */
+    return {
+        init: init
+    };
+
+}());
+
+$( document ).ready( APP.global.init );
