a6f7286f1292ae888bebc0d21e8206804d88e9f0
William Butt <william.butt@starcomww.com>
2014-12-27 21:39:49 -0500

    mobile nav

--- app/pages/home/home.js
+++ app/pages/home/home.js
@@ -5,7 +5,15 @@ var APP = window.APP = window.APP || {};
 APP.home = (function(){
 
     var bindEventsToUI = function() {
-        // ...
+        $('.js-toggle-nav').click(function(e) {
+            e.preventDefault();
+
+            if ($('.mobile-nav').hasClass('active')) {
+                $('.mobile-nav').slideUp().removeClass('active');
+            } else {
+                $('.mobile-nav').slideDown().addClass('active');
+            }
+        });
     };
 
     var init = function() {
